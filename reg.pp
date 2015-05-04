c53ec532d97640f6ad607e205077809aaa2cb5ad
karatyman <vincentmorris76@gmail.com>
2015-05-04 16:26:07 -0400

    Update reg.pp
    Comments

--- reg.pp
+++ reg.pp
@@ -1,3 +1,9 @@
+#MSconfigurator
+#This Manifest defines registry values and makes them real. If a registry value is not defined then it is not real, and will be 
+#exterminated. Doing so keeps prevents Unwanted programs from comming up at startup, keeping the msconfig Clean.
+
+#The following programs are allowed at startup: Alertus, Rainmeter (all other msconfig entries will be deleted)
+
 registry_key {'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run':
 	ensure => present,
 	purge_values => True
